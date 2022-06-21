6298243d90b5e297f7aded21fd43c2072da193c1
Karpov Yuri <yurick@bk.ru>
2022-06-21 18:25:56 +0300

    feat: Создать другие роуты пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch(() => res.send({ message: 'Произошла ошибка' }));
+    .catch((err) => res.send({ message: 'Произошла ошибка' }));
 };
 
 const createUser = (req, res) => {
@@ -12,7 +12,7 @@ const createUser = (req, res) => {
     .then((user) => {
       res.status(201).send({ data: user });
     })
-    .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
+    .catch((err) => res.status(400).send({ message: 'Произошла ошибка' }));
 };
 
 const getUserById = (req, res) => {
@@ -21,11 +21,29 @@ const getUserById = (req, res) => {
     .then((user) => {
       res.status(200).send({ data: user });
     })
-    .catch(() => res.status(400).send({ message: 'Такого пользователя не существует' }));
+    .catch((err) => res.status(400).send({ message: 'Такого пользователя не существует' }));
+};
+
+const updateUser = (req, res) => {
+  const { name, about } = req.body;
+  const userId = req.user._id;
+  User.findByIdAndUpdate(userId, { name, about })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(400).send({ message: 'Произошла ошибка' }));
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  const userId = req.user._id;
+  User.findByIdAndUpdate(userId, { avatar })
+    .then((users) => res.send(users))
+    .catch((err) => res.send({ message: 'Произошла ошибка' }));
 };
 
 module.exports = {
   getUsers,
   createUser,
   getUserById,
+  updateUser,
+  updateAvatar,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
-const { getUsers, getUserById, createUser } = require('../controllers/users');
+const {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  updateAvatar,
+} = require('../controllers/users');
+
+console.log(updateUser);
 
 router.get('/users', getUsers);
 router.get('/users/:userId', getUserById);
 router.post('/users', createUser);
+router.patch('/users/me', updateUser);
+router.patch('/users/me/avatar', updateAvatar);
 
 module.exports = router;
