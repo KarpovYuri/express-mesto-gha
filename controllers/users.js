13c35b26e9a16c1f5cfc4d559644baeaa6321596
Karpov Yuri <yurick@bk.ru>
2022-07-04 23:09:12 +0300

    feat: Доработать контроллер createUser

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 const getUsers = (req, res) => {
@@ -8,15 +9,26 @@ const getUsers = (req, res) => {
 
 // Создание пользоателя
 const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
-      } else {
-        res.status(500).send({ message: 'Ошибка по умолчанию' });
-      }
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      User.create({
+        name,
+        about,
+        avatar,
+        email,
+        password: hash,
+      })
+        .then((user) => res.send({ data: user }))
+        .catch((err) => {
+          if (err.name === 'ValidationError') {
+            res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
+          } else {
+            res.status(500).send({ message: 'Ошибка по умолчанию' });
+          }
+        });
     });
 };
 
