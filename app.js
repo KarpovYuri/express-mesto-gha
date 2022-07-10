f751a1b7c7ac563178f5025abffc5adf1ebc443c
Karpov Yuri <yurick@bk.ru>
2022-07-11 00:08:09 +0300

    feat: add logger.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-err');
 const urlRegExp = require('./utils/url-regexp');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -46,8 +47,12 @@ app.post(
 
 app.use(auth);
 
+app.use(requestLogger);
+
 app.use('/', userRoutes);
 app.use('/', cardRoutes);
+
+app.use(errorLogger);
 
 app.use('*', (req, res, next) => next(new NotFoundError('Страницы не существует')));
 
